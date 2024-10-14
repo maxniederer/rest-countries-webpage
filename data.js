c7ab0453cca18a684b839e19ad0a8ce9b07202a6
maxniederer <66148024+maxniederer@users.noreply.github.com>
2024-10-14 13:09:34 -0400

    preparing country entries for link/filter functionality

--- data.js
+++ data.js
@@ -12,24 +12,34 @@ async function getFullCountryList() {
     json.forEach((e) => {
       const newCountry = document.createElement("li");
       newCountry.classList.add("country-entry");
-      newCountry.innerHTML = `<img class="country-flag" src="${e.flags.png}" alt="${e.flags.alt}" />
-        <div class="country-desc">
-          <h2>${e.name.common}</h2>
-          <ul class="country-stats">
-            <li>
-              <strong>Population:</strong>
-              ${e.population}
-            </li>
-            <li>
-              <strong>Region:</strong>
-              ${e.region}
-            </li>
-            <li>
-              <strong>Capital:</strong>
-              ${e.capital[0]}
-            </li>
-          </ul>
-        </div>`;
+
+      let populationStr = e.population.toLocaleString();
+      let regionStr = e.region;
+      let capitalStr = e.capital.length == 0 ? "N/A" : e.capital.join(", ");
+
+      newCountry.classList.add(regionStr.toLowerCase());
+
+      newCountry.innerHTML = `<a href="#" class="country-link">
+          <img class="country-flag" src="${e.flags.png}" alt="${e.flags.alt}" />
+          <div class="country-desc">
+            <h2>${e.name.common}</h2>
+            <ul class="country-stats">
+              <li>
+                <strong>Population:</strong>
+                ${populationStr}
+              </li>
+              <li>
+                <strong>Region:</strong>
+                ${regionStr}
+              </li>
+              <li>
+                <strong>Capital:</strong>
+                ${capitalStr}
+              </li>
+            </ul>
+          </div>
+        </a>`;
+
       document.getElementById("country-list").appendChild(newCountry);
     });
   } catch (err) {
@@ -48,3 +58,41 @@ getFullCountryList();
 //json.currencies[0].name --- json.currencies[0].symbol
 //json.languages[]
 //json.borders[]
+
+// let link = document.createElement("a");
+// link.href = "#";
+// link.classList.add("country-link");
+
+// let flag = document.createElement("img");
+// flag.src = e.flags.png;
+// flag.alt = e.flags.alt;
+// flag.classList.add("country-flag");
+
+// let desc = document.createElement("div");
+// desc.classList.add("country-desc");
+
+// let name = document.createElement("h2");
+// name.innerHTML = e.name.common;
+
+// let stats = document.createElement("ul");
+// stats.classList.add("country-stats");
+
+// let population = document.createElement("li");
+// let populationVar = e.population;
+// population.innerHTML = `<strong>Population:</strong> ${populationVar}`;
+
+// let region = document.createElement("li");
+// let regionVar = e.region;
+// region.innerHTML = `<strong>Region:</strong> ${regionVar}`;
+
+// let capital = document.createElement("li");
+// let capitalVar = e.capital[0];
+// capital.innerHTML = `<strong>Capital:</strong> ${capitalVar}`;
+
+// stats.appendChild(population);
+// stats.appendChild(region);
+// stats.appendChild(capital);
+// desc.appendChild(name);
+// desc.appendChild(stats);
+// link.appendChild(flag);
+// link.appendChild(desc);
